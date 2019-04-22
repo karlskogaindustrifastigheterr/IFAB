a4e56cdedc2d28369c160e7fefdb99f02342e55a
Erik Lindh <erik.lindh@hotmail.com>
2019-04-22 20:01:13 +0200

    lightbox finished

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,41 +1,90 @@
-	// Open the Modal
-    function openModal(modalId) {
-        document.getElementById(modalId).style.display = "block";
-    }
+var slideIndex = 1;
+var currentModal = null;
 
-    // Close the Modal
-    function closeModal(modalId) {
-        document.getElementById(modalId).style.display = "none";
-    }
+function openModal(modalId) {
+    slideIndex = 1;
+    currentModal = document.getElementById(modalId);
 
-    var slideIndex = 1;
     showSlides(slideIndex);
+    currentModal.getElementsByClassName("lighbox-modal")[0].style.display = "block";
+}
 
-    // Next/previous controls
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
+function closeModal() {
+    currentModal.getElementsByClassName("lighbox-modal")[0].style.display = "none";
+    currentModal = null;
+}
 
-    // Thumbnail image controls
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-        return true;
-    }
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("demo");
-        var captionText = document.getElementById("caption");
-        if (n > slides.length) { slideIndex = 1 }
-        if (n < 1) { slideIndex = slides.length }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-        captionText.innerHTML = dots[slideIndex - 1].alt;
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+    return true;
+}
+
+function showSlides(n) {
+    var i;
+    var slides = currentModal.getElementsByClassName("mySlides");
+
+    var dots = currentModal.getElementsByClassName("demo");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function initLightBoxes(lightboxes) {
+    var template = document.getElementById("lightbox-modal-template");
+    var containerElement = document.getElementById("lightbox-container");
+
+    for (var i = 0; i < lightboxes.length; i++) {
+        lightBoxSettings = lightboxes[i];
+
+        var newElement = document.createElement("div");
+
+        newElement.id = lightBoxSettings.id;
+        newElement.innerHTML = template.innerHTML;
+
+        var imageContainer = newElement.getElementsByClassName("lightbox-slide-image-container")[0];
+
+        imageContainer.innerHTML = getImageHtml(lightBoxSettings.imageUrls);
+
+        var thumbContainer = newElement.getElementsByClassName("lightbox-slide-image-thumb-container")[0];
+
+        thumbContainer.innerHTML = getThumbHtml(lightBoxSettings.imageUrls);
+
+        containerElement.appendChild(newElement);
+    }
+}
+
+function getImageHtml(imageUrls) {
+    var html = "";
+    for (var i = 0; i < imageUrls.length; i++) {
+        var currentImageNumber = i + 1;
+        var currentUrl = imageUrls[i];
+        html += "<div class='mySlides'> " +
+            "<div class='numbertext'>" + currentImageNumber + " / " + imageUrls.length + "</div>" +
+            "<img src='images/" + currentUrl + "' style='width:100%'>" +
+            "</div>"
+    }
+    return html;
+}
+
+function getThumbHtml(imageUrls) {
+    var html = "";
+    for (var i = 0; i < imageUrls.length; i++) {
+        var currentImageNumber = i + 1;
+        var currentUrl = imageUrls[i];
+        html += "<div class='column'>" +
+            "<img class='demo' src='images/" + currentUrl + "' onclick='currentSlide(" + currentImageNumber + ")'>" +
+            "</div>"
+    }
+    return html;
+}
